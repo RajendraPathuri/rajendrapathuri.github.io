11874f5f5f13df87f7bbd32efb115220040ddc6d
Venkata Pathuri <pathurirajendrap@gmail.com>
2025-10-09 01:05:26 +0000

    Change the theme, this is for a cybersecurity professional

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['PT Sans', 'sans-serif'],
-        headline: ['Playfair Display', 'serif'],
+        body: ['Roboto Mono', 'monospace'],
+        headline: ['Source Code Pro', 'monospace'],
         code: ['monospace'],
       },
       colors: {
